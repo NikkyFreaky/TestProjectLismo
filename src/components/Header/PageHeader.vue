3c5ad25176776dd13e7887f09a1e3d5ec6377e5d
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-11-24 23:52:04 +0700

    the svg tag was used instead of img in the header

--- src/components/Header/PageHeader.vue
+++ src/components/Header/PageHeader.vue
@@ -1,43 +1,34 @@
 <script setup>
 import style from './PageHeader.module.less'
+import BaseIcons from '../BaseIcons.vue'
 </script>
 
 <template>
   <header :class="style.header">
     <div :class="style.header__logo">
       <a href="#" :class="style.header__logo_link">
-        <img
-          src="@/assets/svg-sprite/tusur-logo.svg"
-          alt="tusur-logo"
-          :class="style.header__logo_img"
-        />
+        <BaseIcons name="tusur-logo" :class="style.header__logo_img" />
       </a>
     </div>
 
     <div :class="style.header__main">
       <div :class="style.header__control">
         <a href="#">
-          <img
-            src="@/assets/svg-sprite/notification.svg"
-            alt="notification"
-            :class="style.header__icon"
-          />
+          <BaseIcons name="notification" :class="style.header__icon" />
         </a>
         <a href="#">
-          <img src="@/assets/svg-sprite/chat.svg" alt="chat" :class="style.header__icon" />
+          <BaseIcons name="chat" :class="style.header__icon" />
         </a>
         <a href="#">
-          <img src="@/assets/svg-sprite/calendar.svg" alt="calendar" :class="style.header__icon" />
+          <BaseIcons name="calendar" :class="style.header__icon" />
         </a>
       </div>
 
       <div :class="style.header__profile">
         <div :class="style.header__user">
-          <img
-            src="@/assets/svg-sprite/user-profile-icon.svg"
-            alt="profile-icon"
-            :class="style.header__avatar"
-          />
+          <a href="#">
+            <BaseIcons name="user-profile-avatar" :class="style.header__avatar" />
+          </a>
           <a href="#" :class="style.header__user_name">Иван Иванов</a>
         </div>
 
