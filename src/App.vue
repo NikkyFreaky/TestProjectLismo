d13de609d44d03124c9bad1853c1ee6543125d47
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-11-25 01:43:04 +0700

    added the sidebar component and the back to the list of courses button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import PageHeader from './components/header/PageHeader.vue'
+import PageSidebar from './components/sidebar/PageSidebar.vue'
 </script>
 
 <template>
   <div>
     <PageHeader />
+    <PageSidebar />
   </div>
 </template>
---
+++ src/components/buttons/BaseButtons.vue
@@ -0,0 +1,12 @@
+<script setup>
+import style from './BaseButtons.module.less'
+import BaseIcons from '../BaseIcons.vue'
+</script>
+
+<template>
+  <button :class="style.button">
+    <a href="#" :class="style.button__back">
+      <BaseIcons name="arrow-back" />Назад к списку курсов
+    </a>
+  </button>
+</template>
