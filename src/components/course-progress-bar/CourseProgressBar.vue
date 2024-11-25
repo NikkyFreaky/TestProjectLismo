5da077045a7f69ee93733462415322352196003e
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-11-26 00:32:48 +0700

    progress bar has been created for the course

---
+++ src/components/course-progress-bar/CourseProgressBar.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<style scoped src="./CourseProgressBar.less"></style>
+
+<template>
+  <div class="course-progress-bar">
+    <div class="course-progress-bar__current"></div>
+  </div>
+  <span class="course-progress-bar__counter">71%</span>
+</template>
--- src/components/main/PageMain.vue
+++ src/components/main/PageMain.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CourseInfo from '../course-description/CourseDescription.vue'
 import CourseTeachers from '../course-teachers/CourseTeachers.vue'
+import CourseProgressBar from '../course-progress-bar/CourseProgressBar.vue'
 import PageFooter from '../footer/PageFooter.vue'
 </script>
 
@@ -11,9 +12,13 @@ import PageFooter from '../footer/PageFooter.vue'
     <div class="page-main__background"></div>
     <div class="page-main__wrapper">
       <div class="page-main__content">
-        <section class="page-main__header">
+        <section class="page-main__course-description">
           <CourseInfo />
           <CourseTeachers />
+        </section>
+
+        <section class="page-main__course-progress-bar">
+          <CourseProgressBar />
         </section>
       </div>
     </div>
