59e086e95c3f9234a2ac5867fa50a31216100f1d
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-12-04 14:14:33 +0700

    added an adaptive for the page

--- src/components/BaseIcons.vue
+++ src/components/BaseIcons.vue
@@ -456,4 +456,17 @@ export default {
   >
     <circle cx="5" cy="5" r="4.5" stroke="#71809B" />
   </svg>
+
+  <svg
+    v-else-if="name === 'burger-menu'"
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M4 18L20 18" stroke="#A2ACBE" stroke-width="2" stroke-linecap="round" />
+    <path d="M4 12L20 12" stroke="#A2ACBE" stroke-width="2" stroke-linecap="round" />
+    <path d="M4 6L20 6" stroke="#A2ACBE" stroke-width="2" stroke-linecap="round" />
+  </svg>
 </template>
