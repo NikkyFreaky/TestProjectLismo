4bfa6d11ef7ae6d047054ea87196e674dba382d7
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-12-04 15:38:59 +0700

    correction of errors in the display of avatars of additional teachers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
 import Main from './components/main/PageMain.vue'
-import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <template>
   <div>
-    <SpeedInsights />
-
     <Main />
   </div>
 </template>
